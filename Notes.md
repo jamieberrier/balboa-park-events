61ad818bc6a67204faf29bc798a981a5cdcf0000
jamieberrier <berrier.jamie@gmail.com>
2019-04-19 20:42:11 +0000

    harcoded some info to be scraped

--- Notes.md
+++ Notes.md
@@ -1,6 +1,6 @@
 1. GREET USER by displaying
     Welcome to Balboa Park
-    Ever changing. Always amazing.Where culture, science,and nature collide, Balboa Park is home to more than 16 museums, multiple performing arts venues, lovely gardens, trails, and many other creative and recreational attractions, including the San Diego Zoo. With a variety of cultural institutions among its 1,200 beautiful and lushly planted acres, there is something for everyone.
+    Ever changing. Always amazing.Where culture, science, and nature collide, Balboa Park is home to more than 16 museums, multiple performing arts venues, lovely gardens, trails, and many other creative and recreational attractions, including the San Diego Zoo. With a variety of cultural institutions among its 1,200 beautiful and lushly planted acres, there is something for everyone.
 2. DISPLAY TODAY’S EVENTS
     1. Event 1
     2. Event 2
--- lib/balboa_park_events/cli.rb
+++ lib/balboa_park_events/cli.rb
@@ -1,22 +1,24 @@
 # The CLI Controller - responsible for business logic/user interactions
 class BalboaParkEvents::CLI
-  
-  def call 
-    list_todays_events
+
+  def call
+    puts "----- Welcome to Balboa Park -----"
+    puts "Ever changing. Always amazing.Where culture, science, and nature collide, Balboa Park is home to more than 16 museums, multiple performing arts venues, lovely gardens, trails, and many other creative and recreational attractions, including the San Diego Zoo. With a variety of cultural institutions among its 1,200 beautiful and lushly planted acres, there is something for everyone."
+    list
     menu
     goodbye
   end
-  
-  def list_todays_events
-    puts "Today's Events at Balboa Park:"
+
+  def list
     puts <<-DOC.gsub /^\s*/, ''
+    What's Happening Today:
       1. Event 1
       2. Event 2
       3. Event 3
       4. See more/future events
     DOC
   end
-  
+
   def menu
     input = nil
     while input != "exit"
@@ -27,15 +29,17 @@ class BalboaParkEvents::CLI
         puts "More info on 1st event..."
       when "2"
         puts "More info on 2nd event..."
+      when "3"
+        puts "More info on 3rd event..."
       when "list"
-        list_todays_events
+        list
       end
     end
   end
-  
+
   def goodbye
     puts "Come back tomorrow to see the day's events at Balboa Park!"
   end
-  
-  
+
+
 end
