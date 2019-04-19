e1440c72c234ae52205dd01b1a97e59b637790e5
jamieberrier <berrier.jamie@gmail.com>
2019-04-19 20:24:57 +0000

    moved requirements to config/environment.rb

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+#require_relative '../lib/balboa_park_events/scraper'
+#require_relative '../lib/balboa_park_events/event'
+require_relative '../lib/balboa_park_events/cli'
+require_relative '../lib/balboa_park_events/version'
--- lib/balboa_park_events.rb
+++ lib/balboa_park_events.rb
@@ -1,10 +1,10 @@
-require_relative "./balboa_park_events/version"
-require_relative './balboa_park_events/cli'
+#require_relative "./balboa_park_events/version"
+#require_relative './balboa_park_events/cli'
 # Acts as environment
 
-=begin
 module BalboaParkEvents
   class Error < StandardError; end
   # Your code goes here...
 end
-=end
+
+require_relative '../config/environment'
