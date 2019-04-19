e96dc52eadd66a0eaf333db76edbc78d6ff43214
jamieberrier <berrier.jamie@gmail.com>
2019-04-19 17:10:10 +0000

    first commit

---
+++ lib/balboa_park_events.rb
@@ -0,0 +1,6 @@
+require "balboa_park_events/version"
+
+module BalboaParkEvents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/balboa_park_events/version.rb
@@ -0,0 +1,3 @@
+module BalboaParkEvents
+  VERSION = "0.1.0"
+end
