828860e0a8fb75ebb63643a8afa5d251c55db841
jamieberrier <berrier.jamie@gmail.com>
2019-05-02 20:58:27 +0000

    stubbed out event.rb

--- lib/balboa_park_events/event.rb
+++ lib/balboa_park_events/event.rb
@@ -1,4 +1,30 @@
 class BalboaParkEvents::Event
+  attr_accessor :title, :location, :category, :description, :ticket_url, :event_url
 
+  def self.today
+    self.scrape_events
+  end
+
+  def self.scrape_events
+    events = []
+
+    events << self.scrape_event_1
+    events << self.scrape_event_2
+    events << self.scrape_event_3
+
+    events
+  end
+
+  def scrape_event_1
+    event = self.new
+  end
+
+  def scrape_event_2
+    event = self.new
+  end
+
+  def scrape_event_3
+    event = self.new
+  end
 
 end
